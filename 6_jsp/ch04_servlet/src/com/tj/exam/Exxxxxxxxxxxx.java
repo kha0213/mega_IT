b048bcc5dbf49c711ec4580171a4989d3d50ffc6
moon maria <56239469+kha0213@users.noreply.github.com>
2020-02-04 11:54:17 +0900

    11

--- 6_jsp/ch04_servlet/src/com/tj/exam/Exxxxxxxxxxxx.java
+++ 6_jsp/ch04_servlet/src/com/tj/exam/Exxxxxxxxxxxx.java
@@ -1,8 +1,17 @@
 package com.tj.exam;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Exxxxxxxxxxxx {
-	public String solution(int n) {
-	      String answer = "";
-	      return answer;
-	  }
+	public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> qu = new LinkedList<Integer>();
+        int cnt=0;
+        for(int i=0;i<progresses.length;i++) {
+        	qu.offer(progresses[i]);
+        }
+        
+		int[] answer = new int[progresses.length];
+        return answer;
+    }
 }
---
+++ 6_jsp/ch04_servlet/src/com/tj/lect1/Ex3_LifeCycle.java
@@ -0,0 +1,63 @@
+package com.tj.lect1;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ex3_LifeCycle
+ */
+@WebServlet("/Ex3_LifeCycle")
+public class Ex3_LifeCycle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ex3_LifeCycle() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// 객체가 만들어질 때 최초에 한 번 수행
+		System.out.println("Ex3_LifeCycle servlet 객체가 생성될 때 수행(init)");
+	}
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		//client의 요청이 들어올 때
+//		response.getWriter().append("<h1>service() 수행</h1>");
+//	}
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// 객체가 메모리에서 해제(끝낼 때)
+		 System.out.println("servlet종료");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// client의 요청이 get방식으로 들어올 때
+		response.getWriter().append("<h1>doGet() 수행</h1>");
+		System.out.println("doGet() 실행");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// client의 요청이 post방식으로 들어올 때
+		response.getWriter().append("<h1>doPost() 수행</h1>");
+		doGet(request, response);
+	}
+
+}
